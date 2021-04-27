1a77c8cda7abc18257f5d84e232b417c9c638615
Burence1 <burensomondi@gmail.com>
2021-04-27 20:08:49 +0300

    add pizza logic

--- app/main/views.py
+++ app/main/views.py
@@ -4,6 +4,7 @@ from ..models import User, Pizza,Topping
 from flask_login import login_required, current_user
 from .. import db
 from datetime import datetime
+from .forms import AddPizza,SelectPizza
 
 @main.route("/")
 @login_required
@@ -19,4 +20,13 @@ def index():
 
 @main.route("/add_pizza")
 def add_pizza():
-  
+  form = AddPizza()
+  if current_user != "burens":
+    abort(404)
+  elif form.validate_on_submit():
+    pizza=Pizza(name=form.name.data,toppings=form.toppings.data,size=form.size.data)
+    pizza.save_pizza()
+
+    return redirect(url_for("main.index"))
+  title = "New Pizza"
+  render_template ("add_pizza.html",title=title,form=form)
